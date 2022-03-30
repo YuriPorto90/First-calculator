77c0906035b94e33643a3ea1f1c790debd7e7983
Yuri Boaventura Santana Porto <yuri5428@hotmail.com>
2022-03-29 21:50:17 -0300

    v1.1

--- calc.js
+++ calc.js
@@ -1,10 +1,39 @@
 let displayText = ''
 let inputText
 let finalRes = ''
+let locker
 
 function typing(inputText){
-    displayText = displayText + inputText // concatena a string já existente com o valor adquirido
-    document.getElementById("display").value = displayText // printa a string no display
+    if(typeof(inputText) == 'number'){
+        displayText += inputText // concatena a string já existente com o valor adquirido
+        document.getElementById("display").value = displayText // printa a string no display
+        locker = false //atribui false ao bool de trava para a troca de caracteres
+    }
+
+    else{
+        if(locker == false){ //caso o bool seja falso, apenas adiciona o caractere e seta como true
+
+            if(inputText == '^'){
+                
+            }
+
+            else if(inputText == '√'){
+                
+            }
+
+            else{
+                displayText += inputText
+                document.getElementById("display").value = displayText
+                locker = true
+            }
+        }
+
+        else{ //caso o bool seja true, substitui o ultimo caractere que será um operador por outro digitado
+            displayText = displayText.replace(/.$/, inputText)
+            document.getElementById("display").value = displayText
+        }
+
+    }
 }
 
 function res(){
@@ -21,11 +50,12 @@ function clearText(){
 
 function removeLast(){
     if(displayText != finalRes){ //caso o valor do display seja diferente do valor final
-        displayText = displayText.replace(/.$/, ''); //remove o ultimo caractere da string
+        displayText = displayText.replace(/.$/, '') //remove o ultimo caractere da string
         document.getElementById("display").value = displayText //printa a string  no display
     }
 
     else{ //caso o valor do display seja igual ao valor final
         clearText() //chama a função de limpar tela
     }
+    locker = false
 }
